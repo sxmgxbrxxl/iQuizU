12411a2805911e45a16a7dc447b061c182258ea4
Argieeel <argielalap16@gmail.com>
2026-04-16 20:55:57 +0800

    feat: implement App routing, authentication state management, and student layout with sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ import LandingPage from "./pages/general/LandingPage";
 import FeaturesPage from "./pages/general/FeaturesPage";
 import AboutPage from "./pages/general/AboutPage";
 import LoginPage from "./pages/studentSide/LoginPage";
-import SignUpPage from "./pages/studentSide/SignUpPage";
 
 // STUDENT PAGES
 import StudentDashboard from "./pages/studentSide/StudentDashboard";
@@ -235,7 +234,6 @@ function App() {
           }
         />
 
-        <Route path="/signup" element={<SignUpPage />} />
 
         {/* ============================
             ✅ STUDENT ROUTES WITH SIDEBAR
